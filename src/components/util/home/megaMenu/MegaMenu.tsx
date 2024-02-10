b2ab3b82970e37633937c8879fee73c946ffbd2f
Rayy-007 <minkhant88887@gmail.com>
2024-02-10 15:18:10 +0630

    created mega menu final

--- src/components/util/home/megaMenu/MegaMenu.tsx
+++ src/components/util/home/megaMenu/MegaMenu.tsx
@@ -3,192 +3,147 @@ import Link from "next/link";
 const MegaMenu = () => {
   return (
     <>
-      <ul className="menu menu-xs w-full max-w-xs rounded-lg bg-base-200">
-        <li>
-          <Link href="/fashion/woman">Woman's Fashion</Link>
-        </li>
+      <ul className="menu menu-md w-full max-w-xs rounded-lg bg-base-200">
         <li>
           <details open>
-            <summary>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth="1.5"
-                stroke="currentColor"
-                className="h-4 w-4"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M2.25 12.75V12A2.25 2.25 0 014.5 9.75h15A2.25 2.25 0 0121.75 12v.75m-8.69-6.44l-2.12-2.12a1.5 1.5 0 00-1.061-.44H4.5A2.25 2.25 0 002.25 6v12a2.25 2.25 0 002.25 2.25h15A2.25 2.25 0 0021.75 18V9a2.25 2.25 0 00-2.25-2.25h-5.379a1.5 1.5 0 01-1.06-.44z"
-                />
-              </svg>
-              My Files
-            </summary>
+            <summary>Fashion</summary>
             <ul>
-              <li>
-                <a>
+              <li className="group/woman relative">
+                <summary>
+                  Woman's Fashion{" "}
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    strokeWidth="1.5"
-                    stroke="currentColor"
-                    className="h-4 w-4"
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
+                    className="h-5 w-5"
                   >
                     <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      d="M19.5 14.25v-2.625a3.375 3.375 0 00-3.375-3.375h-1.5A1.125 1.125 0 0113.5 7.125v-1.5a3.375 3.375 0 00-3.375-3.375H8.25m2.25 0H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 00-9-9z"
+                      fill-rule="evenodd"
+                      d="M7.21 14.77a.75.75 0 01.02-1.06L11.168 10 7.23 6.29a.75.75 0 111.04-1.08l4.5 4.25a.75.75 0 010 1.08l-4.5 4.25a.75.75 0 01-1.06-.02z"
+                      clip-rule="evenodd"
                     />
                   </svg>
-                  Project-final.psd
-                </a>
+                </summary>
+                <ul className="absolute -right-2/4 hidden rounded-lg bg-base-200 p-5 group-hover/woman:block">
+                  <li>
+                    <Link href="/fashion/woman/shirt">Shirt Fashion</Link>
+                  </li>
+                  <li>
+                    <Link href="/fashion/woman/jeans">Jeans Fashion</Link>
+                  </li>
+                  <li>
+                    <Link href="/fashion/woman/pants">Pants Fashion</Link>
+                  </li>
+                  <li>
+                    <Link href="/fashion/woman/shoes">Shoes Fashion</Link>
+                  </li>
+                  <li>
+                    <Link href="/fashion/woman/bags">Bags Fashion</Link>
+                  </li>
+                </ul>
+              </li>
+
+              <li className="group/man relative">
+                <summary>
+                  Man's Fashion{" "}
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
+                    className="h-5 w-5"
+                  >
+                    <path
+                      fill-rule="evenodd"
+                      d="M7.21 14.77a.75.75 0 01.02-1.06L11.168 10 7.23 6.29a.75.75 0 111.04-1.08l4.5 4.25a.75.75 0 010 1.08l-4.5 4.25a.75.75 0 01-1.06-.02z"
+                      clip-rule="evenodd"
+                    />
+                  </svg>
+                </summary>
+                <ul className="absolute -right-2/4 hidden rounded-lg bg-base-200 p-5 group-hover/man:block">
+                  <li>
+                    <Link href="/fashion/man/shirt">Shirt Fashion</Link>
+                  </li>
+                  <li>
+                    <Link href="/fashion/man/jeans">Jeans Fashion</Link>
+                  </li>
+                  <li>
+                    <Link href="/fashion/man/pants">Pants Fashion</Link>
+                  </li>
+                  <li>
+                    <Link href="/fashion/man/shoes">Shoes Fashion</Link>
+                  </li>
+                  <li>
+                    <Link href="/fashion/man/bags">Bags Fashion</Link>
+                  </li>
+                </ul>
               </li>
               <li>
-                <a>
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    strokeWidth="1.5"
-                    stroke="currentColor"
-                    className="h-4 w-4"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      d="M19.5 14.25v-2.625a3.375 3.375 0 00-3.375-3.375h-1.5A1.125 1.125 0 0113.5 7.125v-1.5a3.375 3.375 0 00-3.375-3.375H8.25m2.25 0H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 00-9-9z"
-                    />
-                  </svg>
-                  Project-final-2.psd
-                </a>
+                <Link href="/fashion/kids">Kids Fashion</Link>
+              </li>
+              <li>
+                <Link href="/fashion/baby">Boy Fashion</Link>
+              </li>
+              <li>
+                <Link href="/fashion/girls">Girl Fashion</Link>
+              </li>
+            </ul>
+          </details>
+          <details>
+            <summary>Electronic</summary>
+            <ul>
+              <li>
+                <Link href="/electronic/laptop">Laptop</Link>
               </li>
               <li>
                 <details open>
-                  <summary>
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      strokeWidth="1.5"
-                      stroke="currentColor"
-                      className="h-4 w-4"
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        d="M2.25 12.75V12A2.25 2.25 0 014.5 9.75h15A2.25 2.25 0 0121.75 12v.75m-8.69-6.44l-2.12-2.12a1.5 1.5 0 00-1.061-.44H4.5A2.25 2.25 0 002.25 6v12a2.25 2.25 0 002.25 2.25h15A2.25 2.25 0 0021.75 18V9a2.25 2.25 0 00-2.25-2.25h-5.379a1.5 1.5 0 01-1.06-.44z"
-                      />
-                    </svg>
-                    Images
-                  </summary>
+                  <summary>Mobile</summary>
                   <ul>
                     <li>
-                      <a>
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          fill="none"
-                          viewBox="0 0 24 24"
-                          strokeWidth="1.5"
-                          stroke="currentColor"
-                          className="h-4 w-4"
-                        >
-                          <path
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            d="M2.25 15.75l5.159-5.159a2.25 2.25 0 013.182 0l5.159 5.159m-1.5-1.5l1.409-1.409a2.25 2.25 0 013.182 0l2.909 2.909m-18 3.75h16.5a1.5 1.5 0 001.5-1.5V6a1.5 1.5 0 00-1.5-1.5H3.75A1.5 1.5 0 002.25 6v12a1.5 1.5 0 001.5 1.5zm10.5-11.25h.008v.008h-.008V8.25zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z"
-                          />
-                        </svg>
-                        Screenshot1.png
-                      </a>
+                      <Link href="/electronic/mobile/iphone">iPhone</Link>
                     </li>
                     <li>
-                      <a>
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          fill="none"
-                          viewBox="0 0 24 24"
-                          strokeWidth="1.5"
-                          stroke="currentColor"
-                          className="h-4 w-4"
-                        >
-                          <path
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            d="M2.25 15.75l5.159-5.159a2.25 2.25 0 013.182 0l5.159 5.159m-1.5-1.5l1.409-1.409a2.25 2.25 0 013.182 0l2.909 2.909m-18 3.75h16.5a1.5 1.5 0 001.5-1.5V6a1.5 1.5 0 00-1.5-1.5H3.75A1.5 1.5 0 002.25 6v12a1.5 1.5 0 001.5 1.5zm10.5-11.25h.008v.008h-.008V8.25zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z"
-                          />
-                        </svg>
-                        Screenshot2.png
-                      </a>
+                      <Link href="/electronic/mobile/samsung">Samsung</Link>
                     </li>
                     <li>
-                      <details open>
-                        <summary>
-                          <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            fill="none"
-                            viewBox="0 0 24 24"
-                            strokeWidth="1.5"
-                            stroke="currentColor"
-                            className="h-4 w-4"
-                          >
-                            <path
-                              strokeLinecap="round"
-                              strokeLinejoin="round"
-                              d="M2.25 12.75V12A2.25 2.25 0 014.5 9.75h15A2.25 2.25 0 0121.75 12v.75m-8.69-6.44l-2.12-2.12a1.5 1.5 0 00-1.061-.44H4.5A2.25 2.25 0 002.25 6v12a2.25 2.25 0 002.25 2.25h15A2.25 2.25 0 0021.75 18V9a2.25 2.25 0 00-2.25-2.25h-5.379a1.5 1.5 0 01-1.06-.44z"
-                            />
-                          </svg>
-                          Others
-                        </summary>
-                        <ul>
-                          <li>
-                            <a>
-                              <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                fill="none"
-                                viewBox="0 0 24 24"
-                                strokeWidth="1.5"
-                                stroke="currentColor"
-                                className="h-4 w-4"
-                              >
-                                <path
-                                  strokeLinecap="round"
-                                  strokeLinejoin="round"
-                                  d="M2.25 15.75l5.159-5.159a2.25 2.25 0 013.182 0l5.159 5.159m-1.5-1.5l1.409-1.409a2.25 2.25 0 013.182 0l2.909 2.909m-18 3.75h16.5a1.5 1.5 0 001.5-1.5V6a1.5 1.5 0 00-1.5-1.5H3.75A1.5 1.5 0 002.25 6v12a1.5 1.5 0 001.5 1.5zm10.5-11.25h.008v.008h-.008V8.25zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z"
-                                />
-                              </svg>
-                              Screenshot3.png
-                            </a>
-                          </li>
-                        </ul>
-                      </details>
+                      <Link href="/electronic/mobile/motorola">Motorola</Link>
+                    </li>
+                    <li>
+                      <Link href="/electronic/mobile/nokia">Nokia</Link>
+                    </li>
+                    <li>
+                      <Link href="/electronic/mobile/huawei">Huawei</Link>
+                    </li>
+                    <li>
+                      <Link href="/electronic/mobile/blackberry">
+                        Blackberry
+                      </Link>
                     </li>
                   </ul>
                 </details>
+              </li>
+              <li>
+                <Link href="/electronic/camera">Camera</Link>
+              </li>
+              <li>
+                <Link href="/electronic/tv">TV</Link>
               </li>
             </ul>
           </details>
         </li>
         <li>
-          <a>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth="1.5"
-              stroke="currentColor"
-              className="h-4 w-4"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M19.5 14.25v-2.625a3.375 3.375 0 00-3.375-3.375h-1.5A1.125 1.125 0 0113.5 7.125v-1.5a3.375 3.375 0 00-3.375-3.375H8.25m0 12.75h7.5m-7.5 3H12M10.5 2.25H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 00-9-9z"
-              />
-            </svg>
-            reports-final-2.pdf
-          </a>
+          <Link href="/home&lifestyle">Home & Lifestyle</Link>
+        </li>
+        <li>
+          <Link href="/furniture">Furniture</Link>
+        </li>
+        <li>
+          <Link href="/sport">Sport & Outdoor</Link>
+        </li>
+        <li>
+          <Link href="/travel">Travel Accessories</Link>
+        </li>
+        <li>
+          <Link href="/health">Health & Beauty</Link>
         </li>
       </ul>
     </>
